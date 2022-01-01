4655f9fd729496ec3538dbcd21235d0c3adad7db
FernandoCelmer <email@fernandocelmer.com>
2022-01-01 04:50:32 -0300

    UPY-#1 - Update Structure

--- runserver.py
+++ runserver.py
@@ -1,9 +1,38 @@
-import os
-from app import app
+# coding=utf-8
 
-if __name__ == "__main__":
+"""
+Runserver
+"""
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+def initialize_app(app):
+    """
+    Application preparation before start
+    """
+    app.config.from_object('config')
+
+    db = SQLAlchemy(app)
+    db.init_app(app)
+
+def main():
+    """
+    Main
+    """
+    initialize_app(app)
     app.run(
         host=app.config['HOST'],
         port=app.config['PORT'],
         debug=app.config['DEBUG']
-    )
+        )
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    main()
